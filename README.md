53092594a5ce0f3d1e879bac87c3412d40fd34c6
MinkuSingh <mailminkusingh@gmail.com>
2025-12-29 18:50:58 +0530

    adding static file

---
+++ README.md
@@ -0,0 +1,1 @@
+https://phonebook-dpl6.onrender.com/info
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan")
 
 const app = express()
 
+app.use(express.static("dist"))
 app.use(express.json())
 morgan.token("body", (req) => (req.method === "POST" ? JSON.stringify(req.body) : ""));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
